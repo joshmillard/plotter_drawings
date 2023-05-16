c7d6367dd07419b0b9cba42580e83440479ecfb6
Josh Millard <josh@joshmillard.com>
2023-05-15 17:21:22 -0700

    Changing writefigure() call to use new keyword arguments.
    Misc cleanup to please PEP8.

--- grid_hatch_r2.py
+++ grid_hatch_r2.py
@@ -1,42 +1,38 @@
-# doing some hatched grids
+""" Draws a grid of randomly cross-hatched squares """
 
 from cortexdraw import *
-
 
 width = 7
 height = 7
 step = 10
 
-fig = plt.figure(figsize=(12, 9), dpi=100, frameon=False)
+fig = plt.figure()
 
 axs = makeaxesgrid(fig, 3)
 
 for a in axs:
     patches = []
-    squareg = getsquaregrid(width,height,step)
+    squareg = getsquaregrid(width, height, step)
 
     for i in range(len(squareg)):
         for j in range(len(squareg[i])):
             # grid gradient variant
-            #lines = circuitpolyline(squareg[i][j],int((i+j)**1.1 + 2))
-            if(random.random() > 0.75): continue
-            lines = crophatch(squareg[i][j],random.random()*2*math.pi, (random.random()*0.3) + 0.5 )
+            # lines = circuitpolyline(squareg[i][j],int((i+j)**1.1 + 2))
+            if random.random() > 0.75:
+                continue
+            lines = crophatch(squareg[i][j], random.random() * 2 * math.pi, (random.random() * 0.3) + 0.5)
             jmag = 0
-            if(random.random() > 0.5):
+            if random.random() > 0.5:
                 jmag = random.random()
-            for l in lines:
-                l = jitter(l,jmag,True)
-                patches.append(mpatches.Polygon(l,closed=False,fill=None,color="black"))
-        
-    x_bounds = [-5, width*step + 5]
-    y_bounds = [-5, height*step + 5]
+            for line in lines:
+                line = jitter(line, jmag, True)
+                patches.append(mpatches.Polygon(line, closed=False, fill=None, color="black"))
 
-    a.set_xlim(x_bounds)
-    a.set_ylim(y_bounds)
+    x_bounds = [-5, width * step + 5]
+    y_bounds = [-5, height * step + 5]
 
     collection = PatchCollection(patches, match_original=True)
     a.add_collection(collection)
 
-
-writefigure(fig)
+writefigure(fig, xbounds=x_bounds, ybounds=y_bounds, filename="grid_hatch", pagesize=[12, 9], drawingsize=[10, 7])
 plt.show()
